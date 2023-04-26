b33cb05c4a59f0b94495f0eb88c15ffbf359922e
kaushal90060 <kisore90060@gmail.com>
2023-04-26 09:56:44 +0530

    Added some changes in background images and Headers

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from  'react-router-dom';
 import App from './App' ;
 import axios from 'axios';
+import './index.css';
 import { AppContextProvider } from './AppContext.js';
 axios.defaults.withCredentials = true;
-
 
 const root = createRoot(document.getElementById("root"));
 root.render(
