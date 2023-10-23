f9112882bb37e06c87f7d0fa460df7329efa4a27
Gaurav Aggarwal <gauravxor@gmail.com>
2023-10-24 02:53:49 +0530

    Refactored the code

--- server/controllers/auth/logoutController.js
+++ server/controllers/auth/logoutController.js
@@ -3,37 +3,35 @@ const AUTH = require('../../helper/authHelper');
 
 const logoutController = async (req, res) => {
 
-	const requestUserId = req.userId;
+    const requestUserId = req.userId;
 
-	const searchCredentialsResult = await AUTH.searchCredentials(requestUserId);
-	if(searchCredentialsResult === null){
-		console.log("LOGOUT CONTROLLER : User credentials not found".red);
-		res.status(404).send({
-			status: "failure",
-			msg: "Credentials not found"
-		});
-	}
-	else{
-		const credentialsDocumentId = searchCredentialsResult._id;
-		const updateLoginStatusResult = await AUTH.updateLoginStatus(credentialsDocumentId, "");
-		if(updateLoginStatusResult !== null){
-			console.log("LOGOUT CONTROLLER : User logged out".yellow);
-			res
-			.clearCookie('accessToken', { httpOnly: true, sameSite: "strict", secure: false})
-			.clearCookie('refreshToken' , { httpOnly: true, sameSite: "strict", secure: false})
-			.status(200).send({
-				status: "success",
-				msg: "Logged out successfully",
-			});
-		}
-		else{
-			console.log("LOGOUT CONTROLLER : Failed to logout".red);
-			res.status(500).send({
-				status: "failure",
-				msg: "Error in updating login/logout status"
-			});
-		}
-	}
+    const searchCredentialsResult = await AUTH.searchCredentials(requestUserId);
+    if (searchCredentialsResult === null) {
+        console.log("LOGOUT CONTROLLER : User credentials not found".red);
+        res.status(404).send({
+            status: "failure",
+            msg: "Credentials not found"
+        });
+    } else {
+        const credentialsDocumentId = searchCredentialsResult._id;
+        const updateLoginStatusResult = await AUTH.updateLoginStatus(credentialsDocumentId, "");
+        if (updateLoginStatusResult !== null) {
+            console.log("LOGOUT CONTROLLER : User logged out".yellow);
+            res
+                .clearCookie('accessToken', { httpOnly: true, sameSite: "strict", secure: false })
+                .clearCookie('refreshToken', { httpOnly: true, sameSite: "strict", secure: false })
+                .status(200).send({
+                    status: "success",
+                    msg: "Logged out successfully",
+                });
+        } else {
+            console.log("LOGOUT CONTROLLER : Failed to logout".red);
+            res.status(500).send({
+                status: "failure",
+                msg: "Error in updating login/logout status"
+            });
+        }
+    }
 };
 
 module.exports = logoutController;
--- server/services/otpCleaner.js
+++ server/services/otpCleaner.js
@@ -3,28 +3,28 @@ const OtpModel = require('../models/otpModel');
 const otpCleaner = async () => {
 
 
-	const otpData = await OtpModel.find({});
+    const otpData = await OtpModel.find({});
 
-	/** If lock OTP collection is not empty */
-	if(!(JSON.stringify(otpData) === JSON.stringify([]))) {
-		otpData.forEach(async (otp) => {
-			const currentTime = new Date();
-			const otpTime = otp.createdAt;
+    /** If lock OTP collection is not empty */
+    if (!(JSON.stringify(otpData) === JSON.stringify([]))) {
+        otpData.forEach(async (otp) => {
+            const currentTime = new Date();
+            const otpTime = otp.createdAt;
 
-			const diff = currentTime - otpTime;
-			const diffInMinutes = diff / 1000;
-			if((diffInMinutes > 150 && otp.otpType === "emailVerification") || (diffInMinutes > 300 && otp.otpType === "passwordReset")) {
-				console.log("OTP Cleaner : Expired OTPs found".yellow);
-				const deleteOtpResult = await OtpModel.deleteOne({ email: otp.email });
-				if(deleteOtpResult === null) {
-					console.log("OTP Cleaner : Error deleting expired OTP data".red);
-				}
-				else{
-					console.log("OTP Cleaner : Expired OTP data deleted".green)
-				}
-			}
-		});
-	}
+            const diff = currentTime - otpTime;
+            const diffInMinutes = diff / 1000;
+            if ((diffInMinutes > 150 && otp.otpType === "emailVerification") || (diffInMinutes > 300 && otp.otpType === "passwordReset")) {
+                console.log("OTP Cleaner : Expired OTPs found".yellow);
+                const deleteOtpResult = await OtpModel.deleteOne({ email: otp.email });
+                if (deleteOtpResult === null) {
+                    console.log("OTP Cleaner : Error deleting expired OTP data".red);
+                }
+                else {
+                    console.log("OTP Cleaner : Expired OTP data deleted".green)
+                }
+            }
+        });
+    }
 }
 
 module.exports = otpCleaner;
