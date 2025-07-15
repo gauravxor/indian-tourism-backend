6979d6959f3347d13fbba27cf80e6273311cab69
Gaurav Aggarwal <gauravxor@gmail.com>
2025-07-16 01:19:57 +0530

    add eslintrc and jsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,24 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 'latest',
     },
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['@root', '.'],
+                    ['@config', './configs'],
+                    ['@controllers', './controllers'],
+                    ['@helpers', './helpers'],
+                    ['@middlewares', './middlewares'],
+                    ['@models', './models'],
+                    ['@routes', './routes'],
+                    ['@services', './services'],
+                    ['@utils', './utils'],
+                ],
+                extensions: ['.js', '.json', '.node'],
+            },
+        },
+    },
     rules: {
         'no-console': 'off',
         'object-shorthand': 'off',
