8643555bcac4f1e3680585605b9f771ec4c551da
Gaurav Aggarwal <gauravxor@gmail.com>
2025-07-16 01:25:03 +0530

    fixed broken imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
             alias: {
                 map: [
                     ['@root', '.'],
-                    ['@config', './configs'],
+                    ['@config', './config'],
                     ['@controllers', './controllers'],
                     ['@helpers', './helpers'],
                     ['@middlewares', './middlewares'],
