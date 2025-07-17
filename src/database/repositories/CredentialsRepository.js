e78ea88434da9396f57ade6ea598cc3d8f643f8e
Gaurav Aggarwal <gauravxor@gmail.com>
2025-07-17 11:21:19 +0530

    add repository functions

--- src/database/repositories/CredentialsRepository.js
+++ src/database/repositories/CredentialsRepository.js
@@ -3,9 +3,14 @@ const logger = require('@config/logger');
 
 class CredentialsRepository {
     static async searchCredentials(userId) {
-        logger.info(`Searching credentails for userId: ${userId}`);
+        logger.info(`Searching credentials for userId: ${userId}`);
         const searchResult = CredentialModel.findOne({ userId: userId });
         return searchResult;
+    }
+
+    static async UpdatePassword(documentId, passwordHash) {
+        const updateResult = await CredentialModel.findByIdAndUpdate(documentId, { password: passwordHash });
+        return updateResult;
     }
 }
 
