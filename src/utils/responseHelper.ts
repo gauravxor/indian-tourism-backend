131f82d1fcd8d880683605120ec8d499bc20b6af
Gaurav Aggarwal <gauravxor@gmail.com>
2025-07-18 11:38:01 +0530

    Typescript migration

---
+++ src/utils/responseHelper.ts
@@ -0,0 +1,27 @@
+import {Response} from 'express';
+
+export function apiResponse(
+    res: Response,
+    statusCode: number = 200,
+    message: string = 'succesdss',
+    data: any = {}
+): Response {
+    return res.status(statusCode).json({
+        message,
+        statusCode,
+        data,
+    });
+}
+
+export function apiError(
+    res: Response,
+    statusCode: number = 500,
+    message: string = 'failure',
+    errors: any = null
+): Response {
+    return res.status(statusCode).json({
+        message,
+        statusCode,
+        errors,
+    });
+}
