bd596e41839e5fed2cf6eb0fea390233e518e562
Gaurav Aggarwal <gauravxor@gmail.com>
2025-07-22 02:15:13 +0530

    Format configs

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,20 +1,20 @@
 const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
 if (!firebasePrivateKey) {
-    throw new Error("FIREBASE_PRIVATE_KEY is not defined");
+    throw new Error('FIREBASE_PRIVATE_KEY is not defined');
 }
 
 const fireBaseConfig: Record<string, string | undefined> = {
     type: process.env.FIREBASE_ACCOUNT_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: firebasePrivateKey.replace(/\\n/g, "\n"),
+    private_key: firebasePrivateKey.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_x509_cert_url: process.env.FIREBASE_X509_CERT_URL,
-    universe_domain: "googleapis.com",
+    universe_domain: 'googleapis.com',
 };
 
 export default fireBaseConfig;
--- config/logger.ts
+++ config/logger.ts
@@ -1,26 +1,22 @@
-import winston from "winston";
-import path from "path";
+import path from 'path';
+
+import winston from 'winston';
 
 const { combine, timestamp, printf, colorize } = winston.format;
 
-const logFormat = printf(
-    ({ level, message, timestamp }) => `${timestamp} [${level}] : ${message}`
-);
+const logFormat = printf(({ level, message, timestamp }) => `${timestamp} [${level}] : ${message}`);
 
 const logger = winston.createLogger({
-    level: "info",
-    format: combine(
-        timestamp({ format: "DD-MM-YYYY HH:mm:ss.SSS" }),
-        logFormat
-    ),
+    level: 'info',
+    format: combine(timestamp({ format: 'DD-MM-YYYY HH:mm:ss.SSS' }), logFormat),
     transports: [
         new winston.transports.File({
-            filename: path.join("logs", "application.log"),
+            filename: path.join('logs', 'application.log'),
         }),
         new winston.transports.Console({
             format: combine(
                 colorize(),
-                timestamp({ format: "DD-MM-YYYY HH:mm:ss.SSS" }),
+                timestamp({ format: 'DD-MM-YYYY HH:mm:ss.SSS' }),
                 logFormat
             ),
         }),
